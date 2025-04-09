fd55a9ed27fabd5067f78d1a7c94993a486c2d57
Inba <rkrishinba@gmail.com>
2025-04-09 15:10:07 +0200

    Dummy CLI

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 FLAGS := -std=c++17 -Wall -Wextra
 
-main: main.o encode.o io.o node.o
+huff: main.o encode.o io.o node.o
 	g++ $(FLAGS) $^ -o main
 
 encode: encode.o node.o
@@ -19,4 +19,4 @@ io.o: src/io.cpp src/io.h
 	g++ $(FLAGS) -c $< -o io.o
 
 clean:
-	rm -rf *.o encode main
+	rm -rf *.o encode huff
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,42 @@
 // CHECK FOR MEMORY LEAKS SOMEHOW
 
 int main(int argc, char* argv[]) {
-  std::cout << argc << std::endl;
+  if (argc < 2) {
+    std::cerr << ("Invalid number of arguments! Try -h for help.") << std::endl;
+    return 1;
+  }
+
+  std::string arg1 = argv[1];
+  // encode
+  if (arg1 == "-e") {
+    if (argc == 4) {
+      std::cout << "Encode file" << std::endl;
+    } else {
+      std::cerr << ("Invalid arguments! Try -h for help.") << std::endl;
+      return 1;
+    }
+
+  // decode
+  } else if (arg1 == "-d") {
+    if (argc == 4) {
+      std::cout << "Decode file" << std::endl;
+    } else {
+      std::cerr << ("Invalid arguments! Try -h for help.") << std::endl;
+      return 1;
+    }
+
+  // help
+  } else if (arg1 == "-h") {
+    std::cout << "./huff:\n";
+    std::cout << "\t-e [SRC] [DEST]: Encode [SRC] to [DEST]\n";
+    std::cout << "\t-d [SRC] [DEST]: Decode [SRC] to [DEST]\n";
+    std::cout << "\t-h : Help Menu" << std::endl;
+  
+  // invalid args
+  } else {
+    std::cerr << ("Invalid arguments! Try -h for help.") << std::endl;
+    return 1;
+  }
+    
   return 0;
 }
