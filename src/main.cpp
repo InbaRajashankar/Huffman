3806cf7d110bfd813fa93a3015149ab3f645fab1
Inba <rkrishinba@gmail.com>
2025-04-12 13:53:56 +0200

    decoding pipeline finished, need to test/debug :)

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 // CHECK FOR MEMORY LEAKS SOMEHOW
 
 void encode_file(const std::string& src, const std::string& dest_huff, const std::string& dest_json);
+void decode_file(const std::string& src_h, const std::string& src_j, const std::string& dest);
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
@@ -17,43 +18,62 @@ int main(int argc, char* argv[]) {
   std::string arg1 = argv[1];
   // encode
   if (arg1 == "-e") {
-    if (argc == 5) {
-      std::string src_path = argv[2];
-      if (src_path.rfind(".txt") != src_path.size() - 4) {
-        std::cerr << "Invalid argument, source should be a .txt file." << std::endl;
-        return 1;
-      }
-
-      std::string dest_h_path = argv[3];
-      if (dest_h_path.rfind(".huff") != dest_h_path.size() - 5) {
-        std::cerr << "Invalid argument, destination 1 should be .huff file." << std::endl;
-        return 1;
-      }
-
-      std::string dest_j_path = argv[4];
-      if (dest_j_path.rfind(".json") != dest_j_path.size() - 5) {
-        std::cerr << "Invalid argument, destination 2 should be a .json file." << std::endl;
-        return 1;
-      }
-
-      std::cout << "Encoding " << src_path << "..." << std::endl;
-      encode_file(src_path, dest_h_path, dest_j_path);
-      std::cout << "Encoded to " << dest_h_path << " with embedding table at ";
-      std::cout << dest_j_path << std::endl;
-
-    } else {
+    if (argc != 5) {
       std::cerr << ("Invalid arguments! Try -h for help.") << std::endl;
       return 1;
     }
+    
+    std::string src_path = argv[2];
+    if (src_path.rfind(".txt") != src_path.size() - 4) {
+      std::cerr << "Invalid argument, source should be a .txt file." << std::endl;
+      return 1;
+    }
+
+    std::string dest_h_path = argv[3];
+    if (dest_h_path.rfind(".huff") != dest_h_path.size() - 5) {
+      std::cerr << "Invalid argument, first destination should be .huff file." << std::endl;
+      return 1;
+    }
+
+    std::string dest_j_path = argv[4];
+    if (dest_j_path.rfind(".json") != dest_j_path.size() - 5) {
+      std::cerr << "Invalid argument, second destination should be a .json file." << std::endl;
+      return 1;
+    }
+
+    std::cout << "Encoding " << src_path << "..." << std::endl;
+    encode_file(src_path, dest_h_path, dest_j_path);
+    std::cout << "Encoded to " << dest_h_path << " with embedding table at ";
+    std::cout << dest_j_path << std::endl;
 
   // decode
   } else if (arg1 == "-d") {
-    if (argc == 5) {
-      std::cout << "Decode file" << std::endl;
-    } else {
+    if (argc != 5) {
       std::cerr << ("Invalid arguments! Try -h for help.") << std::endl;
       return 1;
     }
+
+    std::string src_h_path = argv[2];
+    if (src_h_path.rfind(".huff") != src_h_path.size() - 5) {
+      std::cerr << "Invalid argument, first source should be a .huff file." << std::endl;
+      return 1;
+    }
+
+    std::string src_j_path = argv[3];
+    if (src_j_path.rfind(".json") != src_j_path.size() - 5) {
+      std::cerr << "Invalid argument, second source should be .json file." << std::endl;
+      return 1;
+    }
+
+    std::string dest_path = argv[4];
+    if (dest_path.rfind(".txt") != dest_path.size() - 4) {
+      std::cerr << "Invalid argument, destination should be a .txt file." << std::endl;
+      return 1;
+    }
+
+    std::cout << "Decoding " << src_h_path << "..." << std::endl;
+    decode_file(src_h_path, src_j_path, dest_path);
+    std::cout << src_h_path << "decoded" << std::endl;
 
   // help
   } else if (arg1 == "-h") {
